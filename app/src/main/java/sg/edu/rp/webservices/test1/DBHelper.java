248f4e81a6419781bcb6f7706d6becf4919af19a
michelltansq <19030685@myrp.edu.sg>
2021-08-21 14:47:18 +0800

    update

--- app/src/main/java/sg/edu/rp/webservices/test1/DBHelper.java
+++ app/src/main/java/sg/edu/rp/webservices/test1/DBHelper.java
@@ -98,24 +98,6 @@ public class DBHelper extends SQLiteOpenHelper {
         return result;
     }
 
-//    public ArrayList<String> getLocation() {
-//        ArrayList<String> locations = new ArrayList<String>();
-//        String selectQuery = "SELECT " + COLUMN_ID + "," + COLUMN_LATITUDE + "," + COLUMN_LONGITUDE + "," + COLUMN_LOC_NAME + " FROM " + TABLE_LOCATION;
-//
-//        SQLiteDatabase db = this.getReadableDatabase();
-//        Cursor cursor = db.rawQuery(selectQuery, null);
-//
-//        if (cursor.moveToFirst()) {
-//            do {
-//                locations.add(cursor.getString(0));
-//            } while (cursor.moveToNext());
-//        }
-//        cursor.close();
-//        db.close();
-//
-//        return locations;
-//    }
-
     public ArrayList<Location> getLocations() {
         ArrayList<Location> locations = new ArrayList<Location>();
         String selectQuery = "SELECT " + COLUMN_ID + "," + COLUMN_LATITUDE + "," + COLUMN_LONGITUDE + "," + COLUMN_LOC_NAME + " FROM " + TABLE_LOCATION;
